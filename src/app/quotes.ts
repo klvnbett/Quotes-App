5d7d3cc3390d89420656a168564899915049e4f5
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-18 08:02:14 +0300

    added the boolean description

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,4 +1,5 @@
 export class Quotes {
+    showDescription: boolean;
     constructor(public id: number, public name: string, public description: string, public author:string,public submittedby:string, public completeDate:date){
         this.showDescription=false;
     }
