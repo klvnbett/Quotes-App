779343017e57f885a8d6c34713eb0595f4a1741f
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-20 07:59:29 +0300

    deleted quote, quoteform quotedetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule,} from '@angular/forms'
-
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription: boolean;
-    constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date,public likes:number, public dislikes:number){
-        this.showDescription=false;
-      }
-  }
+  constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date,public likes:number, public dislikes:number){
+      this.showDescription=false;
+    }
+}
