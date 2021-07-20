f5e3c559d691def181b6651e9615d6e2bfaec025
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-20 08:15:03 +0300

    added the general polishing

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input() quote: any;
+  @Output() isComplete = new EventEmitter<boolean>();
 
+  deleteQuote(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  upvote(){
+    this.quote.likes+=1;
+  }
+  downvote(){
+    this.quote.dislikes+=1;
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
-    new Quote (1,'Bett','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
-    new Quote (2,'Wilson','Technology…','Technology… the knack of so arranging  that we don’t have to experience it.','Max Frisch',new Date(2019,7,14),0,0),
-    new Quote (3,'kenneth','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2019,6,3),0,0),
-    new Quote (4,'Karen','Communication','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2019,7,7),0,0),
-    new Quote (5,'Janet','Code and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2019,7,17),0,0),
+    new Quote (1,'Bett','Resentment','resentement is like taking poison and expect the next person to die.','Nelson mandela', new Date(2021,7,16),0,0),
+    new Quote (2,'Wilson','Life','Life has no limitations except the ones you make.','Max Frisch',new Date(2021,7,14),0,0),
+    new Quote (3,'kenneth','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2021,5,23),0,0),
+    new Quote (4,'Karen','Communication','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2021,7,17),0,0),
+    new Quote (5,'Janet','Code and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2021,7,20),0,0),
   ]
   addNewQuote(quote: any) {
     let quoteLength = this.quotes.length;
